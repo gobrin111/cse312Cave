b8b602d7001bfb8f523593c6f1266b0b672c4170
thoma <thomashsu0512@gmail.com>
2024-10-26 20:31:17 -0400

    updated chat frontend

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,10 @@
 let chatMessages = {};
 setInterval(updateChat, 500);
+
 function sendChat() {
     let userInput = document.getElementById("chat-input");
     let userInputValue = userInput.value;
+
     userInput.value = "";
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -14,19 +16,34 @@ function sendChat() {
     request.open("POST", "/chat-messages");
     request.setRequestHeader("Content-Type", "application/json");
     request.send(JSON.stringify(messageJSON));
-    userInput.focus();
-} //this works and is done
 
-function chatMessageHTML(messageJSON) { //adds the message from the db to the chat box
+    userInput.focus();
+}
+
+function chatMessageHTML(messageJSON) {
     const username = messageJSON.username;
     const message = messageJSON.message;
     const messageId = messageJSON.id;
-    const color = messageJSON.color;
-    const like_number = messageJSON.like_number;
-    console.log(messageJSON.like_number);
-    let messageHTML = "<div class='message-container' id='message_"+messageId+"'> <button onclick='deleteMessage(\""+messageId+"\")'>X</button>" + username +": <br/> <button onclick='likeMessage(\""+messageId+"\")'>👍</button> "+like_number+" <div style='background-color: " + color + "'>"+message+"</div> </div>"
+    const from_user = messageJSON.from_user;
+    const like_count = messageJSON.like_count;
+
+    let origin = "other";
+    if (from_user) {
+        origin = "original";
+    }
+
+    let messageHTML = `
+        <div class="message-container" id="message_${messageId}">
+            <button onclick="deleteMessage('${messageId}')"> X </button> ${username} &emsp;
+            <button onclick="likeMessage('${messageId}')"> 👍 </button> ${like_count}
+            <br>
+            <div class="${origin}">
+                ${message}
+            </div>
+        </div>
+        `
     return messageHTML
-} //this is a helper to another function, but this should be done
+}
 
 function deleteMessage(messageId) {
     const request = new XMLHttpRequest();
@@ -39,20 +56,6 @@ function deleteMessage(messageId) {
     request.send();
 }
 
-// function getCookie(name) {
-//   // Create a regular expression to find the cookie by name
-//   const cookieArr = document.cookie.split("; ");
-//
-//   // Loop through the cookie array
-//   for (let i = 0; i < cookieArr.length; i++) {
-//     let cookie = cookieArr[i].trim();
-//     // Check if this cookie matches the name
-//     if (cookie.startsWith(name + "=")) {
-//       return cookie.substring(name.length + 1); // Return the cookie value
-//     }
-//   }
-//   return "none";
-// }
 function likeMessage(messageId){
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -65,6 +68,7 @@ function likeMessage(messageId){
     request.open("POST", "/chat-messages/like/" + messageId);
     request.send();
 }
+
 function addMessageToChat(messageJSON) {
     const chatMessages = document.querySelector('.chat-log');
     chatMessages.insertAdjacentHTML("beforeend", chatMessageHTML(messageJSON))
