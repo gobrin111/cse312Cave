1e0035acf01c6ffe34cd2d7c990c73776abe1002
Michael <8mleung8@gmail.com>
2024-11-29 01:48:17 -0500

    Heroku deployment

--- blueprints/chat.py
+++ blueprints/chat.py
@@ -10,7 +10,7 @@ import os
 chat_bp = Blueprint("chat", __name__)
 
 mongo_uri = os.getenv('MONGO_URI')
-mongo_client = MongoClient(mongo_uri, server_api=ServerApi('1'))
+mongo_client = MongoClient(mongo_uri, server_api=ServerApi('1'), ssl=True)
 db = mongo_client["wurdle"]
 user_collection = db["users"]
 chat_collection = db["chat"]
